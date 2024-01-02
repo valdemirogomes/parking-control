d080bcb825dba743ec5ca53011b6a016355ebc1b
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-01-02 15:23:55 -0300

    ParkingSpotModel entity mapping

---
+++ src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -0,0 +1,42 @@
+package com.api.parkingcontrol.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_parking_spot")
+public class ParkingSpotModel implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	 	@Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private UUID id;
+	    @Column(nullable = false, unique = true, length = 10)
+	    private String parkingSpotNumber;
+	    @Column(nullable = false, unique = true, length = 7)
+	    private String licensePlateCar;
+	    @Column(nullable = false, length = 70)
+	    private String brandCar;
+	    @Column(nullable = false, length = 70)
+	    private String modelCar;
+	    @Column(nullable = false, length = 70)
+	    private String colorCar;
+	    @Column(nullable = false)
+	    private LocalDateTime registrationDate;
+	    @Column(nullable = false, length = 130)
+	    private String responsibleName;
+	    @Column(nullable = false, length = 30)
+	    private String apartment;
+	    @Column(nullable = false, length = 30)
+	    private String block;
+
+}
