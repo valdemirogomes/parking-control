068f1085d669f02be7fe614bc464b42f47cdfa12
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-01-02 22:56:00 -0300

    validating fields

--- src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -8,5 +8,7 @@ import com.api.parkingcontrol.model.ParkingSpotModel;
 
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
-
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
 }
